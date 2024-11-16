584e8308975bb285f87920ec37cf2d71accb2421
melroy1999 <melroydsouza50@gmail.com>
2024-11-16 12:21:21 +0530

    started with the customer apis

--- src/main/java/com/assignment/fooddelivery/service/RestaurantService.java
+++ src/main/java/com/assignment/fooddelivery/service/RestaurantService.java
@@ -3,6 +3,9 @@ package com.assignment.fooddelivery.service;
 import com.assignment.fooddelivery.model.Restaurant;
 import com.assignment.fooddelivery.repository.RestaurantRepository;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,13 @@ import org.springframework.stereotype.Service;
 public class RestaurantService {
     @Autowired
     private RestaurantRepository restaurantRepository;
+    
     public Restaurant getRestaurantById(Long restaurantId) {
         // Fetch restaurant details from database
         return restaurantRepository.findById(restaurantId).orElse(null);
+    }
+    
+    public List<Restaurant> getAllRestaurants(){
+    	return restaurantRepository.findAll();
     }
 }
