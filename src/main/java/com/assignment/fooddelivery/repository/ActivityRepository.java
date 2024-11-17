e7acdb3eede65305cfa56afde1f212334e96225f
Sachin Mejari <sachin.mejari@thyrocare.com>
2024-11-18 01:07:14 +0530

    Revert "Merge branch 'sachinmejari/food-delivery-agent-features' of https://github.com/SachinMejari/food-delivery-api-product- into sachinmejari/food-delivery-agent-features"
    This reverts commit ea56c41dcbec326e69e09be0ee6a3f3ce386b8dc, reversing
    changes made to ac861e69692dfb9dfbd6bf21457ada39a6edb0f4.

--- src/main/java/com/assignment/fooddelivery/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.fooddelivery.repository;
-
-import com.assignment.fooddelivery.model.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
-}
--- src/main/java/com/assignment/fooddelivery/repository/AdminOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.fooddelivery.repository;
-
-import com.assignment.fooddelivery.model.AdminOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminOrderRepository extends JpaRepository<AdminOrder, Long> {
-}
